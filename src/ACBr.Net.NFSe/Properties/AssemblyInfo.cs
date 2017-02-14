c03439606d6e9729b6b710540f314749eff1aef6
RFTD <rfteno@gmail.com>
2017-02-13 21:05:06 -0300

    [+] Adicinado os Schemas no pacote nuget.

--- src/ACBr.Net.NFSe/Properties/AssemblyInfo.cs
+++ src/ACBr.Net.NFSe/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.17044.1624")]
+// [assembly: AssemblyVersion("1.0.17044")]
+[assembly: AssemblyVersion("1.0.17044")]
+[assembly: AssemblyFileVersion("1.0.17044.2359")]
